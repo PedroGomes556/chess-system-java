c096c24d4d6ce08d987a3a613de2c2d3c5e3ecf0
PedroGomes556 <pedrogomesps4xboxone@gmail.com>
2021-12-20 16:47:38 -0300

    First class: Position

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,15 @@
 package aplication;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+
+		Position position = new Position(3, 5);
+		System.out.print(position);
+		
 	}
 
 }

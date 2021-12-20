6ec66bfce40efaf53b9ba337c2c53ddfd76fa4f5
PedroGomes556 <pedrogomesps4xboxone@gmail.com>
2021-12-20 17:32:52 -0300

    Starting to implement Board and Piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,13 @@
 package aplication;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Position position = new Position(3, 5);
-		System.out.print(position);
+		Board board = new Board(8, 8); 
 		
 	}
 
